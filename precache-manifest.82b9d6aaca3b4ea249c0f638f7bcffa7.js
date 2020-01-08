ba2207d22fd361be62f19188dfad242e3af5b910
Danilo Rossi <contact@danilorossi.me>
2020-01-08 20:23:06 +0100

    Updates

---
+++ precache-manifest.82b9d6aaca3b4ea249c0f638f7bcffa7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5c69f8afe52a6f4ce87361dd472c884",
+    "url": "/subway-react-restaurant/index.html"
+  },
+  {
+    "revision": "4368dc6aef3f5566d9eb",
+    "url": "/subway-react-restaurant/static/js/2.2cf463d2.chunk.js"
+  },
+  {
+    "revision": "3d9880ee0da2a100dc4a6223e0aed39a",
+    "url": "/subway-react-restaurant/static/js/2.2cf463d2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c1c716f2d14d557eadfc",
+    "url": "/subway-react-restaurant/static/js/main.f7ef7f62.chunk.js"
+  },
+  {
+    "revision": "36d0b083016ae3770765",
+    "url": "/subway-react-restaurant/static/js/runtime-main.b8455416.js"
+  }
+]);
